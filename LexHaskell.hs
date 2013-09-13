3f270e2f8d154e59a87347fb30ccd05b72c84875
Luis Andres Caviedes Ruiz <andres_luis93@hotmail.com>
2013-09-12 23:55:59 -0500

    reconocimiento de separadores

--- LexHaskell.hs
+++ LexHaskell.hs
@@ -14,8 +14,7 @@ obtenerNumeros x cant =
 							if(not(elem (take (cant-1) (tail x)) (obtenerNumeros (drop cant x) 0)))
 								then [(take (cant-1) (tail x))] ++ obtenerNumeros (drop cant x) 0
 								else obtenerNumeros (drop cant x) 0
-					else obtenerNumeros (drop (cant+1) x) 0		
-					
+					else obtenerNumeros (drop (cant+1) x) 0							
 					
 obtenerPalabras :: String -> Int -> Int ->	[String]
 obtenerPalabras "" cant ban = []
@@ -40,4 +39,21 @@ obtenerPalabras s cant ban =
 							then obtenerPalabras s (cant+1) 1
 							else obtenerPalabras(drop (cant+1) s) 0 ban
 
-							
+obtenerSeparadores :: String -> Int -> Int -> [String]
+obtenerSeparadores "" cant ban = []
+obtenerSeparadores s cant ban =
+	if((s!!cant)=='"' && ban==0)
+		then obtenerSeparadores s (cant+1) 1
+		else if ((s!!cant)=='"' && ban==1)
+			then obtenerSeparadores (drop (cant+1) s) 0 0 
+            else if(not(isLetter(s!!0)) && not(isNumber(s!!0)) && cant==0 && not((s!!0)==' ') && ban==0)
+                then obtenerSeparadores s (cant+1) 0
+				else if(not(isLetter(s!!cant)) && not(isNumber(s!!cant)) && not(cant==0) && not((s!!cant)==' ') && ban==0)
+					then obtenerSeparadores s (cant+1) 0
+					else 
+						if(cant>0)
+						then 
+							if(not(elem (take (cant-ban) s) (obtenerSeparadores(drop (cant+1) s) 0 ban))) 
+								then [(take (cant-ban) s)] ++ obtenerSeparadores(drop (cant+1) s) 0 ban
+								else obtenerSeparadores(drop (cant+1) s) 0 ban   
+						else obtenerSeparadores(drop (cant+1) s) 0 ban
