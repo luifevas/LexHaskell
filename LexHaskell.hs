da21bba450c2e8da1a4e4e3053f83c2772d5056f
Luis Andres Caviedes Ruiz <andres_luis93@hotmail.com>
2013-09-12 23:54:42 -0500

    reconocimiento de palabras

--- LexHaskell.hs
+++ LexHaskell.hs
@@ -16,3 +16,28 @@ obtenerNumeros x cant =
 								else obtenerNumeros (drop cant x) 0
 					else obtenerNumeros (drop (cant+1) x) 0		
 					
+					
+obtenerPalabras :: String -> Int -> Int ->	[String]
+obtenerPalabras "" cant ban = []
+obtenerPalabras s cant ban =
+	if(isLetter(head s) && cant==0 && ban==0)
+		then obtenerPalabras s (cant+1) 0
+		else if((isLetter(s!!cant) || isNumber(s!!cant)) && not(cant==0) && ban==0)
+			then obtenerPalabras s (cant+1) 0
+			else if((head s)=='"' && ban==0)
+				then obtenerPalabras s (cant+1) 1
+				else if((s!!cant)=='"' && ban==1)
+					then
+							if(not(elem (take (cant+1) s) (obtenerPalabras (drop (cant+1) s) 0 0)))
+								then [(take (cant+1) s)] ++ obtenerPalabras (drop (cant+1) s) 0 0
+								else obtenerPalabras (drop (cant+1) s) 0 0
+					else if(not(cant==0) && ban==0)
+						then 
+								if(not(elem (take cant s) (obtenerPalabras(drop (cant+1) s) 0 0)))
+									then [(take cant s)] ++ obtenerPalabras(drop (cant+1) s) 0 0
+									else obtenerPalabras(drop (cant+1) s) 0 0
+						else if(ban==1)
+							then obtenerPalabras s (cant+1) 1
+							else obtenerPalabras(drop (cant+1) s) 0 ban
+
+							
