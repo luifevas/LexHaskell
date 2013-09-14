58db0c6d2f59cabd7d66b866bfb6ebea93174293
Luis Andres Caviedes Ruiz <andres_luis93@hotmail.com>
2013-09-14 18:41:23 -0500

    corrección al momento de sacar los separadores

--- sample.c
+++ sample.c
@@ -1,8 +1,9 @@
-int main ()
+int main (int argc,char **argv)
     { 
         int i ,  sum=0 ; 
-       
+      
         for (i=1.2;  i<=LAST;  i++ ) {
+		
           sum += i ;
         }  
         printf ("sum = %d\n" ,  sum ) ; 
